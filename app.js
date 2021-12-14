5c02d1358d6063fc8cc5b70a3f6788862f1396e1
JONATHAN DOMINGUEZ <jona.dominguez@outlook.com>
2021-12-13 22:25:53 -0600

    mas vistas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.listen(3030,() => console.log('Conexión exitosa'));
+app.listen(3030,() => console.log('Conexión exitosa para INNOVAMOTION'));
 
 app.get('/home', function (req, res) { 
     //res.send ('Bienvenidos al sitio 3030');
@@ -18,3 +18,21 @@ app.get('/blog', function (req, res) {
     res.sendFile(path.join(__dirname, './views/blog.html'));
 });
 
+app.get('/trademarks', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/trademarks.html'));
+});
+app.get('/industrial-designs', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/designs.html'));
+});
+app.get('/fredoom-to-operate-search', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/fto.html'));
+});
+app.get('/patents', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/patents.html'));
+});
+app.get('/regulatory-affairs', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/regulatory.html'));
+});
+app.get('/utility-models', function (req, res) { 
+    res.sendFile(path.join(__dirname, './views/umodels.html'));
+});
